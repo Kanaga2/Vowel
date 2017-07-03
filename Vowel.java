f279484c07adb13f7f2a786de1fcf1a5cb340db9
Kanaga2 <priyakanaga112@gmail.com>
2017-07-03 10:17:48 +0530

    Create Vowel.java

---
+++ Vowel.java
@@ -0,0 +1,21 @@
+# Vowel
+import java.io.*;
+import java.util.*;
+public class Vowel
+{
+public staic void main(String args[])
+{
+char ch;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter a alphabet");
+if((ch='A)||(Ch='E')||(Ch='I')||(Ch='O')||(Ch='U')||(Ch='a')||(ch='e')||(ch='i')||(ch='o')||(ch='u'))
+{
+System.out.println("It is a vowel");
+}
+else
+{
+System.out.println("It is a consonant");
+}
+}
+}
+
